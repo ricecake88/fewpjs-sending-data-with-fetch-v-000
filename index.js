c1bf0d8f411e8f59b431454b1d0545929af8919f
Grace Shih <vocalise@gmail.com>
2020-01-09 05:57:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Add your code here
-<<<<<<< HEAD
 function handleJson(userObject) {
   p = document.createElement("p")
-  console.log(userObject.id)
   p.textContent = userObject.id;
   outputArea = document.getElementById("outputId");
   outputArea.appendChild(p)
@@ -10,8 +8,6 @@ function handleJson(userObject) {
   
 }
 
-=======
->>>>>>> a8e554408cebb32d8b66bafba2831cab0fe97395
 function submitData(username, userEmail) {
   let userData = {
     'name': username,
@@ -25,32 +21,13 @@ function submitData(username, userEmail) {
     method: "POST",
     body: JSON.stringify(userData)
   };
-<<<<<<< HEAD
-  
-   return fetch("http://localhost:3000/users", configObject)
-=======
-
-  function handleJson(userObject) {
-    li = document.createElement(li)
-    li.textContent = userObject.id.value
-    outputArea = document.getElementById("outputId");
-    outputArea.appendChild(li)
-
-
-  }
 
   return fetch("http://localhost:3000/users", configObject)
->>>>>>> a8e554408cebb32d8b66bafba2831cab0fe97395
     .then(function(response) {
       return response.json();
     })
     .then(function(object) {
-<<<<<<< HEAD
-      handleJson(object)
-=======
-      alert(object);
       handleJson(object);
->>>>>>> a8e554408cebb32d8b66bafba2831cab0fe97395
     })
     .catch(function(error) {
       error = document.createElement("p")
